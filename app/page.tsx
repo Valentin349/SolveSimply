a48ebcee5aebcc4797091aaffedd67e969ac8da7
valentin349 <valentino7007@gmail.com>
2023-08-13 23:39:11 +0100

    added hero to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
     <Navbar/>
-    <div className="flex min-h-screen text-4xl items-center justify-center">
-      Hello World
-    </div>
+    <Hero/>
     </>
   )
 }
