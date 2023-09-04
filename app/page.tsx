de7dfb18a259cf30568650905f810b68431067b9
valentin349 <valentino7007@gmail.com>
2023-09-04 15:05:06 +0100

    moved separated out components folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Scene from "./components/Scene";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Scene from "../components/Scene";
 
 export default function Home() {
   return (
