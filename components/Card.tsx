a673c1684ce3fc3cb97b9293da1e1502b2f84bb7
valentin349 <valentino7007@gmail.com>
2023-09-26 17:40:59 +0100

    began making product cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,16 +3,44 @@ import React from "react";
 interface CardProps {
   title: string;
   description: string;
-  vector: React.ReactNode; // Accept an SVG element or any React component
+  headlineItem?: boolean | false;
+  vector: React.ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, vector }) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  headlineItem,
+  vector,
+}) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="p-4">{vector}</div>
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <div className="text-gray-700 text-base">{description}</div>
+    <div className="card">
+      <div
+        className={`max-w-sm rounded overflow-hidden shadow-lg w-[64vw]  ${
+          headlineItem ? "h-[24vh] md:h-[55vh] md:w-[33vw]" : "h-[20vh] md:h-[50vh] md:w-[30vw]"
+        }`}
+      >
+        {headlineItem && (
+          <div className="bg-red-500  text-white text-right p-2 text-xs font-semibold rounded-tl">
+            Popular
+          </div>
+        )}
+
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <div className="text-gray-700 text-base">{description}</div>
+
+          <div className="mt-4">
+            <div className="text-gray-500 font-semibold text-3xl line-through">
+              £1000
+              
+            </div>
+            <div className="text-darkPink font-semibold text-4xl">
+              £999
+              
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Products.tsx
+++ components/Products.tsx
@@ -11,25 +11,68 @@ export default function Products() {
       fullscreen
     >
       <div className="flex justify-center items-center h-screen">
-        <Card
-          title={"Title"}
-          description={"Description"}
-          vector={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <line x1="12" y1="8" x2="12" y2="16" />
-              <line x1="8" y1="12" x2="16" y2="12" />
-            </svg>
-          }
-        />
+        <div className="flex flex-col md:flex-row items-center gap-1">
+          <Card
+            title={"Bespoke Website"}
+            description={"Description"}
+            vector={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <line x1="12" y1="8" x2="12" y2="16" />
+                <line x1="8" y1="12" x2="16" y2="12" />
+              </svg>
+            }
+          />
+
+          <Card
+            title={"SA Chat Assistant"}
+            description={"Description"}
+            headlineItem={true}
+            vector={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <line x1="12" y1="8" x2="12" y2="16" />
+                <line x1="8" y1="12" x2="16" y2="12" />
+              </svg>
+            }
+          />
+
+          <Card
+            title={"Booking System"}
+            description={"Description"}
+            vector={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <line x1="12" y1="8" x2="12" y2="16" />
+                <line x1="8" y1="12" x2="16" y2="12" />
+              </svg>
+            }
+          />
+        </div>
       </div>
     </Html>
   );
