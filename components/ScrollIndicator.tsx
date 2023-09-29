d286846a0dd53ac2123c4c93f1f90024489893de
valentin349 <valentino7007@gmail.com>
2023-09-29 21:03:52 +0100

    scroll indicator stops at end of scroll

--- components/ScrollIndicator.tsx
+++ components/ScrollIndicator.tsx
@@ -1,33 +1,48 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 
 export default function ScrollIndicator() {
   const [isScrolling, setIsScrolling] = useState(false);
+
+  const checkScrollFinished = () => {
+    const element = document.getElementById("ScrollContainer");
+    if (element) {
+      if (element.scrollTop === (element.scrollHeight - window.innerHeight)) {
+        return true;
+      }
+    }
+    return false;
+  };
 
   useEffect(() => {
     let scrollTimeout: NodeJS.Timeout;
 
     const handleWheel = () => {
       setIsScrolling(true);
+
       clearTimeout(scrollTimeout);
 
       // Set a timeout to detect scrolling end
       scrollTimeout = setTimeout(() => {
-        setIsScrolling(false);
+          setIsScrolling(checkScrollFinished);
       }, 1000); // Adjust the timeout duration as needed
     };
 
     window.addEventListener("wheel", handleWheel);
-    window.addEventListener("touchmove", handleWheel)
+    window.addEventListener("touchmove", handleWheel);
 
     return () => {
       window.removeEventListener("wheel", handleWheel);
-      window.removeEventListener("touchmove", handleWheel)
+      window.removeEventListener("touchmove", handleWheel);
     };
   }, []);
 
   return (
-    <div className={`fixed flex justify-center items-center bottom-12 inset-x-0 transition-opacity ${isScrolling ? 'opacity-0' : 'opacity-100'}`}>
+    <div
+      className={`fixed flex justify-center items-center bottom-12 inset-x-0 transition-opacity ${
+        isScrolling ? "opacity-0" : "opacity-100"
+      }`}
+    >
       <div className="w-8 h-16 rounded-full border-2 border-lightPink p-1 relative">
         <div className="w-5 h-5 rounded-full bg-lightPink absolute top-1 animate-scroll-tailwind"></div>
       </div>
